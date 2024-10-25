d227c3cfb4cd25d98daeddc6f3be01105b1bdf70
senseiwells <wellsm488@gmail.com>
2024-10-25 16:20:21 +0100

    Remove kt serialization from toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
         .split("+")[0]
 
     kotlin("jvm").version(jvmVersion)
-    alias(libs.plugins.kotlin.serialization)
+    kotlin("plugin.serialization").version(jvmVersion)
     alias(libs.plugins.fabric.loom)
     alias(libs.plugins.mod.publish)
     `maven-publish`

e815793d67cde6623a6b7f0c50afbdd9fe65f97f
senseiwells <wellsm488@gmail.com>
2024-12-04 19:20:08 +0000

    Bump version & changelog

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     java
 }
 
-val modVersion = "0.3.0"
+val modVersion = "0.3.1"
 val releaseVersion = "${modVersion}+${libs.versions.minecraft.get()}"
 version = releaseVersion
 group = "me.senseiwells"
@@ -86,7 +86,7 @@ tasks {
         file = remapJar.get().archiveFile
         changelog.set(
             """
-            - Update to 1.21.3
+            - Update to 1.21.4
             """.trimIndent()
         )
         type = STABLE

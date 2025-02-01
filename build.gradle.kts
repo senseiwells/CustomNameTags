393188a0042c9e050e5b2c8a6521f88a0eedbd97
senseiwells <wellsm488@gmail.com>
2025-02-01 12:07:49 +0000

    Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     java
 }
 
-val modVersion = "0.3.3"
+val modVersion = "0.3.4"
 val releaseVersion = "${modVersion}+${libs.versions.minecraft.get()}"
 version = releaseVersion
 group = "me.senseiwells"
@@ -86,7 +86,7 @@ tasks {
         file = remapJar.get().archiveFile
         changelog.set(
             """
-            - Updated internal apis
+            Fix compatability with fsit
             """.trimIndent()
         )
         type = STABLE
